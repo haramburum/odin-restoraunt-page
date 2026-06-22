e9b56eceadcf6d6d3b17dba8eac96412f331bfb7
Andrey Sidorkin <gholos@yandex.ru>
2026-06-22 16:36:45 +0700

    Add webpack merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,39 +1,26 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from "node:path";
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "js/main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css'
-    })
   ],
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
-          filename: 'img/[hash][ext]'
-        }
+          filename: 'img/[hash][ext]',
+        },
       },
     ],
   },
